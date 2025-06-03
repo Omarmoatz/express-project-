da49daf87cca1a1d51b26e5149fb384a7e18f7a7
Omarmoatz <omar.raouf.002@gmail.com>
2025-06-03 14:39:53 +0300

    add create endpoint with validations

--- index.js
+++ index.js
@@ -1,37 +1,61 @@
 const express = require("express");
+const { body, validationResult } = require("express-validator");
 
 const app = express()
 
+app.use(express.json())  // middleware for body request 
+
 const courses = [
     {
-        "id":1,
-        "title":"js course",
-        "price":102
+        "id": 1,
+        "title": "js course",
+        "price": 102
     },
     {
-        "id":2,
-        "title":"ts course",
-        "price":1000
+        "id": 2,
+        "title": "ts course",
+        "price": 1000
     },
 ]
 
 
-app.get("/api/courses", (req, res)=>{
+app.get("/api/courses", (req, res) => {
     res.json(courses)
 })
 
 
-app.get("/api/courses/:courseId", (res, req) => {
-    const courseId = +res.params.courseId
+app.get("/api/courses/:courseId", (req, res) => {
+    const courseId = +req.params.courseId
+    const course = courses.find((course) => course.id === courseId)
 
-    console.log(courseId);
-    
-    const course = courses.find(courses.id === courseId)
-    
-    res.json(course)
+    if (!course) {
+        res.status(404).json({ "error": "Course not found" })
+    }
+    res.json(course);
 })
 
 
+app.post("/api/courses",
+    body('title')
+        .notEmpty()
+        .withMessage("title is required")
+        .isLength({ min: 4 })
+        .withMessage("title must not be less than 4 chars"),
+    body('price')
+        .notEmpty()
+        .withMessage("price is required ")
+        .isNumeric()
+        .withMessage("the price must be numbers"),
+    (req, res) => {
+
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors.array())
+        }
+
+        courses.push({ "id": courses.length + 1, ...req.body })
+        res.json(courses)
+    })
 
 
 
@@ -42,7 +66,6 @@ app.get("/api/courses/:courseId", (res, req) => {
 
 
 
-
-app.listen(4000, ()=>{
+app.listen(4000, () => {
     console.log("listening on port 4000");
 })
